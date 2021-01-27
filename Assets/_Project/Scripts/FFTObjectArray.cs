5e1b600ea1b34fad2417785020c8c20aa285e329
Brad Hammond <brad@xy01.net>
2021-01-27 22:46:59 +1100

    FFT Cube ready to go

--- Assets/_Project/Scripts/FFTObjectArray.cs
+++ Assets/_Project/Scripts/FFTObjectArray.cs
@@ -6,25 +6,54 @@ public class FFTObjectArray : MonoBehaviour
 {
     public FrequencyBandAnalyser _FFT;
     public FrequencyBandAnalyser.Bands _FreqBands = FrequencyBandAnalyser.Bands.Eight;
-    FFTScale[] _FFTObjects;
+    GameObject[] _FFTGameObjects;
 
-    public FFTScale _ObjectToSpawn;
+    public GameObject _ObjectToSpawn;
 
     public float _Spacing = 1;
 
     public Vector3 _ScaleStrength = Vector3.up;
+    Vector3 _BaseScale;
 
     // Start is called before the first frame update
     void Start()
     {
-        _FFTObjects = new FFTScale[(int)_FreqBands];
+        _FFTGameObjects = new GameObject[(int)_FreqBands];
+        _BaseScale = _ObjectToSpawn.transform.localScale;
+        //---   LINEAR
+        for (int i = 0; i < (int)_FreqBands; i++)
+        {
+            GameObject newFFTObject = Instantiate(_ObjectToSpawn);
+            newFFTObject.transform.SetParent(transform);
+            newFFTObject.transform.localPosition = new Vector3(_Spacing * i, 0, 0);
+            _FFTGameObjects[i] = newFFTObject;
+        }
 
-        ////---   LINEAR
-        //for (int i = 0; i < (int)_FreqBands; i++)
+
+        //float angleSpacing = (2f * Mathf.PI) / _FFTObjects.Length;
+        ////---   CIRCULAR
+        //for (int i = 0; i < _FFTObjects.Length; i++)
         //{
+        //    float angle = i * angleSpacing;
+        //    float x = Mathf.Sin(angle) * _Spacing;
+        //    float y = Mathf.Cos(angle) * _Spacing;
+
+
         //    FFTScale newFFTObject = Instantiate(_ObjectToSpawn).GetComponent<FFTScale>();
         //    newFFTObject.transform.SetParent(transform);
-        //    newFFTObject.transform.localPosition = new Vector3(_Spacing * i, 0, 0);
+        //    newFFTObject.transform.localPosition = new Vector3(x, y, 0);
+
+        //    FFTSetMaterialFloat setMaterialFloat = newFFTObject.gameObject.GetComponentInChildren<FFTSetMaterialFloat>();
+        //    if(setMaterialFloat != null)
+        //    {
+        //        setMaterialFloat._FFT = _FFT;
+        //        setMaterialFloat._FreqBands = _FreqBands;
+        //        setMaterialFloat._FrequencyBandIndex = i;
+        //    }
+
+        //    //---   ROTATION
+        //    newFFTObject.transform.LookAt(transform.position);
+        //    newFFTObject.transform.localRotation *= Quaternion.Euler(-90,0,0);
 
         //    newFFTObject._FFT = _FFT;
         //    newFFTObject._FreqBands = _FreqBands;
@@ -33,39 +62,14 @@ public class FFTObjectArray : MonoBehaviour
 
         //    _FFTObjects[i] = newFFTObject;
         //}
+    }
 
-
-        float angleSpacing = (2f * Mathf.PI) / _FFTObjects.Length;
-        //---   CIRCULAR
-        for (int i = 0; i < _FFTObjects.Length; i++)
+    private void Update()
+    {
+        //---   LINEAR
+        for (int i = 0; i < _FFTGameObjects.Length; i++)
         {
-            float angle = i * angleSpacing;
-            float x = Mathf.Sin(angle) * _Spacing;
-            float y = Mathf.Cos(angle) * _Spacing;
-
-
-            FFTScale newFFTObject = Instantiate(_ObjectToSpawn).GetComponent<FFTScale>();
-            newFFTObject.transform.SetParent(transform);
-            newFFTObject.transform.localPosition = new Vector3(x, y, 0);
-
-            FFTSetMaterialFloat setMaterialFloat = newFFTObject.gameObject.GetComponentInChildren<FFTSetMaterialFloat>();
-            if(setMaterialFloat != null)
-            {
-                setMaterialFloat._FFT = _FFT;
-                setMaterialFloat._FreqBands = _FreqBands;
-                setMaterialFloat._FrequencyBandIndex = i;
-            }
-
-            //---   ROTATION
-            newFFTObject.transform.LookAt(transform.position);
-            newFFTObject.transform.localRotation *= Quaternion.Euler(-90,0,0);
-
-            newFFTObject._FFT = _FFT;
-            newFFTObject._FreqBands = _FreqBands;
-            newFFTObject._FrequencyBandIndex = i;
-            newFFTObject._ScaleStrength = _ScaleStrength;
-
-            _FFTObjects[i] = newFFTObject;
+            _FFTGameObjects[i].transform.localScale = _BaseScale + (_ScaleStrength * _FFT.GetBandValue(i, _FreqBands));
         }
     }
 }
